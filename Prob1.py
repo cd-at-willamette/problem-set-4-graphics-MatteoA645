efee797c5352ecbed2ec7ad1ba5cded82b388303
MatteoA645 <mralemany@willamette.edu>
2024-10-14 13:04:06 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,23 +1,36 @@
 ############################################################
-# Name:
+# Name: Matteo
 # Name(s) of anyone worked with:
-# Est time spent (hr):
+# Est time spent (hr): 1 hour
 ############################################################
 
 from pgl import GWindow, GRect, GOval, GLine, GLabel
 
-# Setting up initial window dimensions. 
-# You can change these if you want. They are in number of pixels.
-WIDTH = 600
-HEIGHT = 600
 
 def draw_image():
     """ Creates a window and draws a student's creation"""
 
     # Creating the window
-    gw = GWindow(WIDTH, HEIGHT)
-    
-    # And now it is your turn! Add your code below! Make sure you meet all the requirements!
+    gw = GWindow(500, 500)
+    w = 500
+    o = GOval(w//5,w//5,200,200)
+    o.set_filled(True)
+    o.set_fill_color("yellow")
+    gw.add(o)
+    o2=GOval(125,125,150,150)
+    o2.set_filled(True)
+    o2.set_fill_color("blue")
+    gw.add(o2)
+    l=GLine(129,129,271,271)
+    l2=GLine(129,271,271,129) #hated finding these coordinates with a passion
+    gw.add(l)
+    gw.add(l2)
+    t=GLabel("Uncanny X-Men",w//5+30,w//10)
+    r=GRect(100,100,200,200)
+    r.set_filled(False)
+    r.set_color("blue")
+    gw.add(t)
+    gw.add(r)
 
 
 
